31d66d6cb604608056750e2143c6102189429906
Gabriel <gabrielmaestre79@gmail.com>
2024-05-19 00:25:51 -0400

    fix

--- components/cta/CallToActionButton.tsx
+++ components/cta/CallToActionButton.tsx
@@ -3,11 +3,9 @@ import { SiLinkedin } from "react-icons/si";
 
 export const CallToActionButton = () => {
   return (
-    <button className="justify-start rounded-md border py-3 px-4 sm:justify-center lg:justify-start hover:border-zinc-400 transition-all">
-      <a target="blank" href="https://www.linkedin.com/in/gabriel-maestre-31b52529b/"  className="text-base tracking-tight flex gap-2 items-center justify-center">
-        <SiLinkedin size={16} />
-        LinkedIn
-      </a>
-    </button>
+    <a target="blank" href="https://www.linkedin.com/in/gabriel-maestre-31b52529b/" className="text-base tracking-tight flex gap-2 items-center justify-center rounded-md border py-3 px-4 sm:justify-center lg:justify-start hover:border-zinc-400 transition-all">
+      <SiLinkedin size={16} />
+      LinkedIn
+    </a>
   )
 }
