e55c8154d32af5e67bc5429f0e979b5991c803d6
Gabriel <gabrielmaestre79@gmail.com>
2024-05-22 10:10:44 -0400

    fix

--- components/stack/MainStack.tsx
+++ components/stack/MainStack.tsx
@@ -5,11 +5,11 @@ export const MainStack = () => {
     <div className="flex flex-col">
       <span className="uppercase text-sm tracking-wide font-medium text-zinc-500">Main stack:</span>
       <div className="flex gap-4 mt-3 [&>*]:text-zinc-600">
-        <SiTailwindcss className="hover:text-black transition-all cursor-pointer" title="Tailwind CSS" size={26} />
-        <SiJavascript  className="hover:text-black transition-all cursor-pointer" title="Javascript" size={26} />
-        <SiTypescript  className="hover:text-black transition-all cursor-pointer" title="Typescript" size={26} />
-        <SiReact  className="hover:text-black transition-all cursor-pointer" title="React" size={26} />
-        <SiNextdotjs  className="hover:text-black transition-all cursor-pointer" title="Next.js" size={26} />
+        <SiTailwindcss className="hover:text-black transition-all" title="Tailwind CSS" size={26} />
+        <SiJavascript  className="hover:text-black transition-all" title="Javascript" size={26} />
+        <SiTypescript  className="hover:text-black transition-all" title="Typescript" size={26} />
+        <SiReact  className="hover:text-black transition-all" title="React" size={26} />
+        <SiNextdotjs  className="hover:text-black transition-all" title="Next.js" size={26} />
       </div>
     </div>
   )
