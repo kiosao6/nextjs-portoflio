433ff547bd8bb5f5c55404cb25491388fa1f053e
Gabriel <gabrielmaestre79@gmail.com>
2024-06-27 23:28:59 -0400

    test

--- app/page.tsx
+++ app/page.tsx
@@ -7,7 +7,7 @@ export default function Home() {
     <main>
       <section className="px-8 mt-12 xl:px-0 mb-12 max-w-7xl mx-auto lg:flex">
         <div>
-          <p className="text-[80px] max-w-4xl mb-6 md:text-8xl font-medium leading-tight tracking-tighter lg:text-[145px] lg:leading-none">I&apos;m Gabriel Maestre.</p>
+          <p className="text-[80px] max-w-4xl mb-6 md:text-8xl font-medium leading-tight tracking-tighter lg:text-[145px] lg:leading-none">I’m Gabriel Maestre.</p>
           <h1 className="tracking-tight leading-7 text-[15px] mb-6 lg:text-xl lg:leading-relaxed max-w-3xl text-neutral-600">I develop front-end websites using React and Next.js with meticulous attention to detail. Passionate about frontend technologies and seamless user interfaces. Available for work.</h1>
           <div className="flex justify-start items-center gap-4 mb-6">
             <CallToActionButton />
